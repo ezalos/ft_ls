3e5dcac36810418808d87a62bf2607d81e15878d
ezalos <ezalos@gmail.com>
2020-09-28 14:32:01 +0200

    stat bug investigation

--- srcs/print/6_file_link.c
+++ srcs/print/6_file_link.c
@@ -6,7 +6,7 @@
 /*   By: ezalos <ezalos@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/28 09:51:33 by ezalos            #+#    #+#             */
-/*   Updated: 2020/09/28 09:51:43 by ezalos           ###   ########.fr       */
+/*   Updated: 2020/09/28 14:31:45 by ezalos           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,17 @@
 void	print_file_link(t_sys_files *file)
 {
 	struct stat		stat_link;
-	char			*buf;
-	size_t			bufsiz = 500;
+	char			buf[500];
 
 	if (S_IFLNK == (file->statbuf.st_mode & S_IFMT))
 	{
 		if (stat(file->path, &stat_link) != 0 /*SUCCESS*/)
 		{
+			ft_printf("%s\n", file->path);
 			perror(ERROR_DIR_STAT);
 			return ;
 		}
-		buf = ft_memalloc(bufsiz);
-		if (readlink(file->path, buf, bufsiz) == -1)
+		if (readlink(file->path, buf, 500) == -1)
 			return ;
 		ft_printf(" -> ");
 		print_file_color(stat_link, buf);
