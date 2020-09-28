c25c5dbbd4498a4b976c7db0be2a881fd0ff13cd
ezalos <ezalos@gmail.com>
2020-09-28 15:34:45 +0200

    Keeping colors when possible

--- srcs/print/6_file_link.c
+++ srcs/print/6_file_link.c
@@ -6,7 +6,7 @@
 /*   By: ezalos <ezalos@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/28 09:51:33 by ezalos            #+#    #+#             */
-/*   Updated: 2020/09/28 15:32:25 by ezalos           ###   ########.fr       */
+/*   Updated: 2020/09/28 15:34:21 by ezalos           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,25 @@ void	print_file_link(t_sys_files *file)
 {
 	struct stat		stat_link;
 	char			buf[500];
+	int				color;
 
 	if (S_IFLNK == (file->statbuf.st_mode & S_IFMT))
 	{
-		// if (stat(file->path, &stat_link) != 0 /*SUCCESS*/)
-		// {
-		// 	ft_printf("%s\n", file->path);
-		// 	perror(ERROR_DIR_STAT);
-		// 	return ;
-		// }
+		color = 0;
+		if (stat(file->path, &stat_link) != 0 /*SUCCESS*/)
+		{
+			// ft_printf("%s\n", file->path);
+			// perror(ERROR_DIR_STAT);
+			color = 1;
+		}
 		ft_bzero(buf, 500);
 		if (readlink(file->path, buf, 500) == -1)
 			return ;
 		ft_printf(" -> ");
-		print_file_color(stat_link, buf);
+		if (color)
+			print_file_color(stat_link, buf);
 		print_file_name(buf);
-		ft_printf("%~{}");
+		if (color)
+			ft_printf("%~{}");
 	}
 }
